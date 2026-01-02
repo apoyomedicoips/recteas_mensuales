b0a85815329afd228b22123ff3bad88c1591f6c1
apoyomedicoips <apoyomedicoips@gmail.com>
2026-01-02 07:18:52 -0300

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,1 +1,20 @@
+// docs/config.js
+const CONFIG = {
+  DATA_DIR: "data",
+
+  THRESHOLDS: {
+    CRIT_DAYS: 7,
+    WARN_DAYS: 14
+  },
+
+  DEFAULTS: {
+    SELECT_ALL_ITEMS: true
+  },
+
+  AUTH: {
+    SESSION_KEY: "recetas_auth_v1",
+    USER_BADGE_KEY: "recetas_auth_user_v1",
+    STOCK_KEY: "recetas_stock_v1"
+  }
+};
 
